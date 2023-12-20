eb5ba544ada229b64bfd1296b405497be2081cf1
francoisdmq <francois.domecq@ratp.fr>
2023-12-20 16:02:04 +0100

    parse to camelKeys

--- api/app/domains/establishments/data-access/index.ts
+++ api/app/domains/establishments/data-access/index.ts
@@ -1,15 +1,20 @@
+import { camelKeys } from "js-convert-case";
 import { Knex } from "knex";
 
 import database from "@student-sphere-infrastructure/database";
 import { logger } from "@student-sphere-root/config";
-import { Establishment } from "@student-sphere-root/scripts/import-establishments/types/establishment";
+import {
+    Establishment,
+    EstablishmentDbRow
+} from "@student-sphere-root/scripts/import-establishments/types/establishment";
 import { ESTABLISHMENTS_TABLE_NAME } from "@student-sphere-root/utils/table-names";
 
 const findEstablishmentsByName = async (establishmentName: string | undefined): Promise<Establishment[]> => {
     try {
-        return await database(ESTABLISHMENTS_TABLE_NAME)
-            .select("establishment_id", "establishment_name", "establishment_postal_code", "establishment_city", "establishment_type")
+        const establishmentsDbRow: EstablishmentDbRow[] = await database(ESTABLISHMENTS_TABLE_NAME)
+            .select("establishment_id", "establishment_name", "establishment_postal_code", "establishment_city_name", "establishment_type")
             .modify((qb: Knex.QueryBuilder) => qb.whereILike("establishment_name", `%${establishmentName}%`));
+        return establishmentsDbRow.map((establishmentDbRow: EstablishmentDbRow) => <Establishment>camelKeys(establishmentDbRow));
     } catch (err) {
         logger.info(`Could not find establishment with name : ${establishmentName}`);
         throw err;
